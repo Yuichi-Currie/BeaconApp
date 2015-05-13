f2f83a0e7b706b2aec0204f9daab685b56e8115b
Yuichi <yuichi.currie@gmail.com>
2015-05-13 15:44:07 +0100

    Basic Beacon Function.
    Finds Closest Beacon and Displays Information.
    Closest Beacon Function Untested.
    
    Following Tutorial Used:
    http://www.software7.com/blog/creating-a-beacon-app-for-android-in-less-than-10-minutes-from-scratch/

--- MyApplication/app/src/main/java/microwatt/beacon_app/MainActivity.java
+++ MyApplication/app/src/main/java/microwatt/beacon_app/MainActivity.java
@@ -1,24 +1,131 @@
 package microwatt.beacon_app;
 
+import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements BeaconConsumer {
+
+
+    public static final String TAG = "Beacon Info";
+    private BeaconManager beaconManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        beaconManager.bind(this);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void onBeaconServiceConnect () {
+        final Region region = new Region("Allowed Beacons",Identifier.parse("01122334-4556-6778-899a-abbccddeeff0"), null,null);
+
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                try {
+                    Log.d(TAG, "DidEnterRegion");
+                    beaconManager.startRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                try {
+                    Log.d(TAG, "DidExitRegion");
+                    beaconManager.stopRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+
+            }
+        });
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                String beaconinfo;
+                Double minimumdistance = Double.MAX_VALUE;
+                Beacon closestbeacon = null ;
+                for (Beacon onebeacon : beacons) {
+                    if (onebeacon.getDistance() < minimumdistance){
+                        closestbeacon=onebeacon;
+                    }
+
+                    beaconinfo = "distance: " + closestbeacon.getDistance() + " id: " + closestbeacon.getId1() + "  id 2: " + closestbeacon.getId2() + " id 3: " + closestbeacon.getId3();
+
+                    Log.d(TAG, beaconinfo);
+                    final String finalBeaconinfo = beaconinfo;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            TextView beacondisplay = (TextView) findViewById(R.id.Beacon_ID);
+                            beacondisplay.setText(finalBeaconinfo);
+
+                        }
+                    });
+                }
+
+
+            }
+
+        });
+
+        try{
+                beaconManager.startMonitoringBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
